1b32a8e93a3e77414cc608a4ed67def4becf2e27
Brucepk <brucelee_gui@163.com>
2019-03-16 16:24:44 +0800

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,26 @@
+﻿'''
+作者：pk哥
+公众号：Python知识圈
+日期：2018/09/10
+代码解析详见公众号「Python知识圈」。
+
+'''
+
+
+# 将多个文本内容合并成一个
+# 获取目标文件夹的路径
+filedir = 'E:\\CET46\\CET4\\txt'
+# 获取当前文件夹中的文件名称列表
+filenames = os.listdir(filedir)
+# 打开当前目录下的result.txt文件，如果没有则创建
+f = open('E:\\CET46\\CET4\\txt\\result4.txt', 'w', encoding='UTF-8')
+# 先遍历文件名
+for filename in filenames:
+    filepath = filedir + '\\' + filename
+    # 遍历单个文件，读取行数
+    for line in open(filepath, encoding='UTF-8'):
+        f.writelines(line)
+    f.write('\n')
+# 关闭文件
+f.close()
+
---
+++ wordcount.py
@@ -0,0 +1,42 @@
+﻿import re
+import os
+
+'''
+作者：pk哥
+公众号：Python知识圈
+日期：2018/09/10
+代码解析详见公众号「Python知识圈」。
+
+'''
+
+
+def WordList(file):
+    with open(file, encoding='UTF-8') as f:
+        text = f.read()
+        return text.split()
+
+
+def save2txt(path, data):
+    print('正在保存信息')
+    with open(path + 'count.txt', 'a', encoding='utf-8')as f:
+        for i in data:
+            f.write(str(i))
+            f.write('\n')
+        print('保存成功！')
+
+
+file = 'E:\\CET46\\CET6\\txt\\result6.txt'
+result = WordList(file)
+words = [i.lower() for i in re.findall('[a-zA-Z]+', str(result)) if len(i) > 2]
+d = dict()
+common_word = ['the', 'and', 'for', 'are', 'that', 'you', 'not', 'one', 'this', 'people']
+for word in words:
+    if word not in common_word:
+        d[word] = d.get(word, 0) + 1    # 获取 key 的值，默认为 0
+        data = sorted(d.items(), key=lambda x: x[1], reverse=True)
+path = 'E:\\CET46\\CET6\\txt\\'
+save2txt(path, data)
+
+
+
+
